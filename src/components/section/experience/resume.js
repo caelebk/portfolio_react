d5c66c62769f2d2cd8ce5a596ff1c7fe31f1704b
Caeleb <khocaeleb@gmail.com>
2022-09-05 22:30:49 -0700

    Updated

--- src/components/section/experience/resume.js
+++ src/components/section/experience/resume.js
@@ -7,7 +7,7 @@ export default function Resume() {
         <section className="resume">
             <div className="resume-container" data-aos="zoom-in">
                 <div className="line" data-aos="expand"></div>
-                <a href="https://drive.google.com/file/d/1peh2pBIg4T_2DWMbwP--sVwo1OncvvPa/view?usp=sharing" target="_blank" className='resume-link'> Resume </a>
+                <a href="https://drive.google.com/file/d/1peh2pBIg4T_2DWMbwP--sVwo1OncvvPa/view?usp=sharing" rel="noreferrer" target="_blank" className='resume-link'> Resume </a>
                 <div className="line"></div>
             </div>
         </section>
