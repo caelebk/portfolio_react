1a0a625fd5c94d1be8843397d33afe48e30cd846
caeleb <khocaeleb@gmail.com>
2023-04-10 00:19:44 -0700

    Added github pages support

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import NavBar from './components/navbar/navbar';
 import ScrollMenu from './components/scroll-menu/scroll-menu';
 import ScrollAnchor from './components/scroll-menu/scroll-anchor';
 import Introduction from './components/section/introduction/introduction'
-import reportWebVitals from './reportWebVitals';
 import About from './components/section/about/about';
 import Experience from './components/section/experience/experience';
 import Resume from './components/section/experience/resume';
@@ -31,8 +30,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
