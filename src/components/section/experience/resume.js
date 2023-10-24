636dc9cccaf051ca2b1e9956ad354d380a22d109
caelebk <khocaeleb@gmail.com>
2023-10-23 17:38:17 -0700

    update resume icon

--- src/components/section/experience/resume.js
+++ src/components/section/experience/resume.js
@@ -6,15 +6,14 @@ import { BsFillPersonLinesFill } from 'react-icons/bs'
 export default function Resume() {
     return (
         <section className="resume">
-            <div className="resume-container" data-aos='fade-up'>
-                <BsFillPersonLinesFill className='icon highlight' />
-                <a href="https://drive.google.com/file/d/1p8rY3Q4PFVN7xAMTLRUK4J3IBd5x33MX/view?usp=sharing"
-                    rel="noreferrer"
-                    target="_blank"
-                    className='resume-link hvr-hang'>
+            <a className="resume-container hvr-hang" 
+                data-aos='fade-up'
+                href="https://drive.google.com/file/d/1p8rY3Q4PFVN7xAMTLRUK4J3IBd5x33MX/view?usp=sharing"
+                rel="noreferrer"
+                target="_blank">
+                <BsFillPersonLinesFill className='icon' />
                     Resume
-                </a>
-            </div>
+            </a>
         </section>
     );
 }
